7e13730b4f48b6f0bb6938a6cef4e58cc44a0e68
jjikkobro <0831ww59@gmail.com>
2025-11-11 20:56:06 +0900

    Fix: Use NEXT_PUBLIC_API_BASE_URL and remove rewrites

--- next.config.js
+++ next.config.js
@@ -4,14 +4,6 @@ const nextConfig = {
     domains: ['res.cloudinary.com'],
     formats: ['image/webp', 'image/avif'],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.API_BASE_URL}/:path*`,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
